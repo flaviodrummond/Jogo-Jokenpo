d738d3c99318ccc7aae1334749d857859cc7148d
flaviodrummond <flaviosax20@gmail.com>
2025-05-20 11:46:06 -0300

    Criado a função playthegamer para fazer a verificação dos resultados e colocar na tela

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const result = document.querySelector('.result')
+
 const playHuman = (humanChoice) => {
-    console.log(humanChoice)
+    playTheGamer(humanChoice, playMachine())
 }
 
 const playMachine = () => {
     const choise = ['stone', 'paper', 'scissors']
 
-    return choise
+    const random = Math.floor(Math.random() * 3)
+
+    return choise [random]
 }
+
+ const playTheGamer = (human, machine) => {
+
+    if(human === machine) {
+        result.innerHTML = 'Deu empate'
+    } else if(human === 'paper' && machine === 'stone' || human === 'stone' && machine === 'scissors' || human === 'scissors' && machine === 'paper') {
+        result.innerHTML = 'Você Ganhou'
+    } else {
+        result.innerHTML = 'Você perdeu'
+    }
+
+ }
